44ab8257f19f445c49855bdbe276740a597d522d
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-12 12:02:21 -0700

    Adds mapping

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,15 +1,22 @@
 class Complement
   VERSION = 3
   def self.of_dna(rna)
-    case rna
-    when 'C'
-      'G'
-    when 'G'
-      'C'
-    when 'T'
-      'A'
-    when 'A'
-      'U'
+    letters_array = rna.chars
+    complement = []
+    
+    letters_array.map do |letter| 
+      if letter == 'G'
+        complement.push('C')
+      elsif letter == 'C'
+        complement.push('G')
+      elsif letter == 'T'
+        complement.push('A')
+      elsif letter == 'A'
+        complement.push('U')
+      end
     end
+
+puts complement.class
+    complement.to_s
   end
 end
--- ruby/rna-transcription/rna_transcription_test.rb
+++ ruby/rna-transcription/rna_transcription_test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative 'rna_transcription'
 
 # Test data version:
