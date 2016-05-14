1b0cbf2346f73be2e0fccb33b63bfa4ce7ddd623
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-13 21:48:24 -0700

    Adds conditionals for numbers with 2 root prime factors

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,17 +2,12 @@ class Raindrops
   def self.convert(number)
     if number == 1
       number = "#{number}"
-    elsif number == 3
+    elsif number % 3 == 0
       number = "Pling"
-    elsif number == 5
+    elsif number % 5 == 0
       number = "Plang"
-    elsif number == 7
+    elsif number % 7 == 0
       number = "Plong"
-
-      #  I think it might be a good idea to
-      #  use operators and conditionals to
-      #  calculate the test of the prime numbers.
-      #  for example: if number == 3 || number == 2
     end
   end
 end
--- ruby/raindrops/raindrops_test.rb
+++ ruby/raindrops/raindrops_test.rb
@@ -25,27 +25,22 @@ class RaindropsTest < Minitest::Test
   end
 
   def test_6
-    skip
     assert_equal 'Pling', Raindrops.convert(6)
   end
 
   def test_9
-    skip
     assert_equal 'Pling', Raindrops.convert(9)
   end
 
   def test_10
-    skip
     assert_equal 'Plang', Raindrops.convert(10)
   end
 
   def test_14
-    skip
     assert_equal 'Plong', Raindrops.convert(14)
   end
 
   def test_15
-    skip
     assert_equal 'PlingPlang', Raindrops.convert(15)
   end
 
