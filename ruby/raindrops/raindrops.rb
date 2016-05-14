f07794c14b6b17c23138cb53e11e5dfd66f1ef4d
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-13 22:19:58 -0700

    Passes two more tests

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,6 +2,10 @@ class Raindrops
   def self.convert(number)
     if number == 1
       number = "#{number}"
+    elsif (number / 3) % 5 == 0
+      number = "PlingPlang"
+    elsif (number / 3) % 7 == 0
+      number = "PlingPlong"
     elsif number % 3 == 0
       number = "Pling"
     elsif number % 5 == 0
--- ruby/raindrops/raindrops_test.rb
+++ ruby/raindrops/raindrops_test.rb
@@ -45,17 +45,14 @@ class RaindropsTest < Minitest::Test
   end
 
   def test_21
-    skip
     assert_equal 'PlingPlong', Raindrops.convert(21)
   end
 
   def test_25
-    skip
     assert_equal 'Plang', Raindrops.convert(25)
   end
 
   def test_35
-    skip
     assert_equal 'PlangPlong', Raindrops.convert(35)
   end
 
