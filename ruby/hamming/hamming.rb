e2eaafdc40ac256a4b49991b268f6b1eef214495
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-13 21:14:18 -0700

    Fixes spacing

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,16 +1,16 @@
 class Hamming
-  VERSION = 1
-	def self.compute(value_1, value_2)
+VERSION = 1
+  def self.compute(value_1, value_2)
     count = 0
     Range.new(0, value_1.length-1).to_a.each do |index|
-  		if value_1[index] == value_2[index]
-  			count += 0
-  		elsif value_1.length != value_2.length
+      if value_1[index] == value_2[index]
+        count += 0
+      elsif value_1.length != value_2.length
         raise ArgumentError.new("length difference")
-      else 
-  			count += 1
-  		end
+      else
+        count += 1
+      end
     end
     count
-	end
+  end
 end
