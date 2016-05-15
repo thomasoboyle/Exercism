403e610281cba458c196bd126002e5c1e8326a23
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-15 11:24:00 -0700

    Working on passing first test

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,7 @@
+class Squares
+  def initialize(x)
+  end
+
+  def square_of_sum
+  end
+end
--- ruby/difference-of-squares/difference_of_squares_test.rb
+++ ruby/difference-of-squares/difference_of_squares_test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative 'difference_of_squares'
 
 # Test data version:
