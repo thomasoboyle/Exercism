35e866793c289a1635354abfeab713fef8857490
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-16 19:22:35 -0700

    My solution to squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,15 +1,21 @@
 class Squares
+  VERSION = 2
+  
   def initialize(number)
     @number = number
   end
 
   def square_of_sum()
-    (1..@number).reduce(:+)**2
+    (0..@number).reduce(:+)**2
   end
 
   def sum_of_squares()
-    sum = (1..@number).to_a.reduce(0) {|acc, elm| 
+    sum = (0..@number).to_a.reduce(0) {|acc, elm| 
       acc += elm ** 2
     }
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
   end
 end
