342a49dc52e65b64a7cd36c345885aadfce54c81
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-18 21:33:11 -0700

    Replaces @number with attr_reader: number

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,16 +1,17 @@
 class Squares
   VERSION = 2
+  attr_reader :number
   
   def initialize(number)
     @number = number
   end
 
   def square_of_sum()
-    (0..@number).reduce(:+)**2
+    (0..number).reduce(:+)**2
   end
 
   def sum_of_squares()
-    sum = (0..@number).to_a.reduce(0) {|acc, elm| 
+    sum = (0..number).to_a.reduce(0) {|acc, elm| 
       acc += elm ** 2
     }
   end
