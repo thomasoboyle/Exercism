00ff89588b475e32f0da561665b78b7ab652cbce
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-23 12:40:09 -0700

    Small tweek

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,9 +2,9 @@ class Pangram
   def self.is_pangram?(str)
     if str.include? ""
       is_pangram = true
-    elsif str.include? ("a".."z").to_a
+    
+    elsif str.include?("a".."z".to_a)
       is_pangram = true
     end
   end
-
 end
