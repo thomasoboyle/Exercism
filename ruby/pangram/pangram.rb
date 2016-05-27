c7832a1db96bfd3b222c926d36247033d8c8d657
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-05-26 20:29:48 -0700

    Adds unless

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,13 +1,15 @@
 class Pangram
   def self.is_pangram?(str)
-  
-  ("a".."z").each do |l|
-  if str.include?(l).nil?
-    is_pangram = false
-  elsif str.include?("")
-    is_pangram = true
-  else
-    is_pangram = true
+
+    unless str.include?
+      ("a".."z").to_a.each do |i|
+        if str.include?(l).nil?
+          is_pangram = false
+        else
+          is_pangram = true
+        end
+      end
+    end
   end
 end
 
