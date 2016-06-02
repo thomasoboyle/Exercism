5f6a2613e539699331e4a602d09b198da059a54b
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-06-01 18:39:37 -0700

    Looks for errors

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,13 +1,12 @@
 class Pangram
   def self.is_pangram?(str)
-
-    unless str.include? ""
-      ("a".."z").to_a.each do |i|
-        if str.include?(l).nil?
-          is_pangram = false
-        else
-          is_pangram = true
-        end
+    input = str.chars
+    pan = ("a".."z").to_a
+    missing_letters = pan.map do |letter|
+      unless input.include?(letter)
+        letter
       end
-    end
-
+    end.compact
+    is_pangram = missing_letters.empty?
+  end
+end
