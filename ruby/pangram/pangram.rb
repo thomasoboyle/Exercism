585cfa035829e86fb02e0c0019f8e058c281aeec
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-06-02 18:16:26 -0700

    Uses select and include

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,12 +1,9 @@
 class Pangram
   VERSION = 1
+
   def self.is_pangram?(string)
     input = string.downcase
-    pangram = ("a".."z")
-    pangram.map { |letter|
-      unless input.include?(letter)
-        letter
-      end
-    }.compact.empty?
+    ("a".."z").select { |letter| !input.include? letter }.compact.empty?
   end
+  
 end
