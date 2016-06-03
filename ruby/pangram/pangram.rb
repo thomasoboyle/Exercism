c6bd279848906671c9d0a7f92d25623fcd0bd924
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-06-02 22:29:28 -0700

    Removes compact

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -3,7 +3,7 @@ class Pangram
 
   def self.is_pangram?(string)
     input = string.downcase
-    ("a".."z").select { |letter| !input.include? letter }.compact.empty?
+    ("a".."z").select { |letter| input.reject letter }.empty?
   end
   
 end
