f507fbe9c2c904224d3b260e8ac3d5c821d368eb
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-06-06 13:01:21 -0700

    Whiteboarding sieve ideas

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -2,7 +2,14 @@ class Sieve
   def initialize(number)
   end
 
-  def primes()
-    primes = []
+primes_array = []
+not_primes = []
+
+  def primes(2..number) each do |n| n+1
+    if new_num.prime = true
+      primes_array.push(new_num)
+    else
+      not_primes.push(new_num)
+    end
   end
 end
