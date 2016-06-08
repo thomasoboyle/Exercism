9a5332ad01b0139db3a5f3a11dbd4d7a2c1df6ac
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-06-07 18:44:23 -0700

    Removes comments

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -7,21 +7,17 @@ class Sieve
   def primes()
     not_primes = []
     range = (2..@picked_number).to_a
+    
     range.map {|number|
-      # go from 2 to 3
       (number..@picked_number).each do |n|
         new_number = number * n
-        # this multiples the number
+        
         if new_number < @picked_number + 1
           not_primes.push(new_number)
-          # not prime numbers in array
         end
-      end
 
-        # Ensure that each new number is less than picked number
-        # store each number in a new array
-        # then chech if each item is prime. Return primes.
-      }
-      primes = range - not_primes
-    end
+      end
+    }
+    primes = range - not_primes
+  end
 end
