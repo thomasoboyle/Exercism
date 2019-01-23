91d504571098fd07152a471e1748f1431dd53bb1
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-01-22 19:40:12 -0800

    Remove conditional by using a better default value

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,11 +1,7 @@
 require 'pry'
 
 class TwoFer
-  def self.two_fer(name=nil)
-    if name
-      "One for #{name}, one for me."
-    else
-      "One for you, one for me."
-    end
+  def self.two_fer(person='you')
+   "One for #{person}, one for me."
   end
 end
