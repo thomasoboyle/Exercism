3f3df5fcc6b49208e63608732f2c9c6c5b2d09b1
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-02-13 20:33:59 -0800

    Initial setup and start of the solution

---
+++ ruby/clock/clock.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Clock
+  attr_accessor :hour, :minute
+
+  def initialize(hour:, minute: 0) 
+    @hour = hour
+    @minute = minute
+  end
+
+  def to_s
+    build_hour(hour) + ':' + build_minute(hour)
+  end
+
+  def build_hour(hour)
+    if hour > 12
+      hour = 24 - hour
+      build_hour(hour)
+    elsif hour < 10
+      '0' + hour.abs.to_s
+     else
+       hour.to_s
+     end
+  end
+
+  def build_minute(hour)
+    if minute< 10
+      '0' + minute.to_s
+    else
+      'hi'
+    end
+  end
+end
