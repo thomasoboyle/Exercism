c46e50fba80d35959c0f7bd0486c0bf6c984f772
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-02-16 23:50:59 -0800

    Initial set up for triangle exercise

---
+++ ruby/triangle/triangle.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Triangle
+  attr_reader :first_side, :second_side, :third_side
+
+  def initialize(side_lengths)
+    @first_side = side_lengths[0]
+    @second_side = side_lengths[1]
+    @third_side = side_lengths[2]
+  end
+
+  def equilateral?
+    return true if first_side == second_side && first_side && third_side
+  end
+end
