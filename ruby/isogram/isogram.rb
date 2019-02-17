97b954428259dc6bc1ce95446a44b1eb0087c6b4
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-02-17 15:42:31 -0800

    My solution to isogram

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,9 +1,19 @@
-require 'pry'
-
 class Isogram
   def self.isogram?(string)
     return true if string == ''
-    length = string.length
+    is_isogram(string)
+  end
+
+  def self.is_isogram(string)
+    string = strip_string(string)
     string.downcase.split('').uniq.length == string.length
+  end
+
+  def self.strip_string(string)
+    if string.include?('-') || string.include?(' ')
+      string.sub(/[\s+-]/, '')
+    else
+      string
+    end
   end
 end
--- ruby/isogram/isogram_test.rb
+++ ruby/isogram/isogram_test.rb
@@ -54,19 +54,16 @@ class IsogramTest < Minitest::Test
   end
 
   def test_made_up_name_that_is_an_isogram
-    skip
     input = "Emily Jung Schwartzkopf"
     assert Isogram.isogram?(input), "Expected true, '#{input}' is an isogram"
   end
 
   def test_duplicated_character_in_the_middle
-    skip
     input = "accentor"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_same_first_and_last_characters
-    skip
     input = "angola"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
