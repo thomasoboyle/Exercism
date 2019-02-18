810e20d7a4fdf1b9ce3119b4a02fb5af9b1403a1
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-02-18 15:18:51 -0800

    Generate rand letters using alphabet hash

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,9 +1,47 @@
 class Robot
   attr_reader :name
 
-
   def initialize
-    @name = "AB123"
+    @name = gen_rand_letters + gen_rand_nums
   end
 
+  def gen_rand_letters
+    ALPHABET[rand(1..26)] + ALPHABET[rand(1..26)]
+  end
+
+  def gen_rand_nums
+    rand(100..999).to_s
+  end
+
+  def self.forget
+  end
+
+  ALPHABET = {
+    1 => 'A',
+    2 => 'B',
+    3 => 'C',
+    4 => 'D',
+    5 => 'E',
+    6 => 'F',
+    7 => 'G',
+    8 => 'H',
+    9 => 'I',
+    10 => 'J',
+    11 => 'K',
+    12 => 'L',
+    13 => 'M',
+    14 => 'N',
+    15 => 'O',
+    16 => 'P',
+    17 => 'Q',
+    18 => 'R',
+    19 => 'S',
+    20 => 'T',
+    21 => 'U',
+    22 => 'V',
+    23 => 'W',
+    24 => 'X',
+    25 => 'Y',
+    26 => 'Z',
+  }
 end
