63bfb368e6c9b24de92fde47feb98426bba19aaf
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-02-24 18:37:01 -0800

    Use one attr_accessor

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,31 +1,39 @@
 # Clock solution
 class Clock
-  attr_accessor :hour, :minute
+  attr_accessor :minute
 
   def initialize(hour: 0, minute: 0)
-    roll_over_hour, @minute = minute.divmod(60)
-    @hour = (hour + roll_over_hour) % 24
+    @minute = hour * 60 + minute
   end
 
   def to_s
-    format('%02d:%02d', hour, minute)
+    format('%02d:%02d', divided_hours, divided_minutes)
+  end
+
+  def divided_hours
+    hours = minute / 60
+    hours % 24
+  end
+
+  def divided_minutes
+    minute % 60
   end
 
   def +(other)
-    Clock.new(hour: hour + other.hour, minute: minute + other.minute)
+    Clock.new(hour: divided_hours, minute: divided_minutes + other.minute)
   end
 
   def -(other)
-    Clock.new(hour: hour - other.hour, minute: minute - other.minute)
+    Clock.new(hour: divided_hours, minute: divided_minutes - other.minute)
   end
 
   def ==(other)
     self.class === other &&
-      hour == other.hour &&
-      minute == other.minute
+      self.divided_hours == other.divided_hours &&
+      self.divided_minutes == other.divided_minutes
   end
 
   def hash
-    hour.hash ^ minute.hash
+    minute.hash
   end
 end
