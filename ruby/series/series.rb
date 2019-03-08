2c030816f2edf72f68e992afbf75320c8060ed79
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-03-08 09:54:49 -0800

    Start work for each_cons

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,16 +1,27 @@
 require 'pry'
 
 class Series
-  attr_accessor :series
-
   def initialize(series)
     @series = series
   end
 
   def slices(slice_length)
-    number_slices = series.length - slice_length + 1
     raise ArgumentError if slice_length > series.length
+    number_slices = series.length - slice_length + 1
 
-    number_slices.times.map { |i| series[i, slice_length] }
+    # number_slices.times.map { |i| series[i, slice_length] }
+    
+    string = []
+    series.split('').each_cons(slice_length) do |a|
+      string << a
+      string.join
+      binding.pry
+    end
+    string
   end
+
+  private
+
+  attr_accessor :series
+
 end
