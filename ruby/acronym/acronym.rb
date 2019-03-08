54052f9f5737cc783969f2c9387e2c376c691c34
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-03-07 21:41:22 -0800

    use .join

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,11 +1,8 @@
+require 'pry'
+
 class Acronym
   def self.abbreviate(string)
     string = string.scan(/\b\w/)
-    acronym = ""
-
-    string.each do |letter|
-      acronym << letter[0]
-    end
-    acronym.upcase
+    string.join.upcase
   end
 end
