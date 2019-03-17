073206adaabc21b956b1161058af0c31517e25ba
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-03-17 07:31:31 -0700

    Remove logic from initialize

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,9 +1,18 @@
 class Phrase
   def initialize(phrase)
-    @phrase = phrase.downcase.scan(/\b[\w']+\b/)
+    @phrase = phrase
   end
 
   def word_count
-    @phrase.each_with_object(Hash.new(0)) { |word, count| count[word] += 1 }
+    words.each_with_object(Hash.new(0)) { |word, count| count[word] += 1 }
   end
+
+  private
+
+  attr_reader :phrase
+
+  def words
+    phrase.downcase.scan(/\b[\w']+\b/)
+  end
+
 end
