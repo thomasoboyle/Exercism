75daa8cf87cb71a96cdad21c43b252f53bb543fc
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-06-28 21:08:01 -0700

    Remove columns from attr_reader

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,13 +1,16 @@
 class Matrix
-  attr_reader :columns, :numbers
+  attr_reader :numbers
 
   def initialize(numbers)
     @numbers = numbers
-    @columns = rows.transpose
   end
 
   def rows
     numbers.lines.map { |row| row.split.map(&:to_i) }
+  end
+
+  def columns
+    rows.transpose
   end
 end
 
