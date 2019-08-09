32d4d4cc44a01444fed813f81a76fd1968df770a
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-08-08 21:46:15 -0700

    Subtract 9

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,21 +1,23 @@
+require 'pry'
+
 class Luhn
 
   def self.valid?(card_number)
     numbers = card_number.gsub(/\D/, '').reverse
 
-    sum, i = 0, 0
+    sum, index = 0, 0
 
     numbers.each_char do |number|
       num = number.to_i
 
       # double odd numbers
-      num *= 2 if num.odd?
+      num *= 2 if index.odd?
 
-      # add remainder
-      num = 1 + (num - 10) if num >= 10
-
+      # subtract 9 if sum is double digit
+      num = num - 9 if num >= 10
       sum += num
-      i += i
+
+      index += index
     end
 
     return if sum.zero?
