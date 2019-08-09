80d96bd7dc2fc37a159102208ff5f15134310538
Thomas O'Boyle <thomashoboyle@gmail.com>
2019-08-08 21:33:05 -0700

    Legwork

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,6 +1,24 @@
 class Luhn
 
-  def self.valid?(number)
-    false
+  def self.valid?(card_number)
+    numbers = card_number.gsub(/\D/, '').reverse
+
+    sum, i = 0, 0
+
+    numbers.each_char do |number|
+      num = number.to_i
+
+      # double odd numbers
+      num *= 2 if num.odd?
+
+      # add remainder
+      num = 1 + (num - 10) if num >= 10
+
+      sum += num
+      i += i
+    end
+
+    return if sum.zero?
+    (sum % 10).zero?
   end
 end
