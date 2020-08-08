8f58e47560f4d98cfe978cf8382ee45f07cb94d7
Thomas O'Boyle <thomashoboyle@gmail.com>
2020-08-08 09:38:38 -0700

    Use array

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,19 +1,19 @@
 class ResistorColorDuo
 
   def self.value(colors)
-    number = "#{ColorValues[colors[0]]}" + "#{ColorValues[colors[1]]}"
-    number[0..1].to_i
+    "#{ColorArray.index(colors[0])}#{ColorArray.index(colors[1])}".to_i
   end
 
-  ColorValues = {
-    "black" => 0,
-    "brown" => 10,
-    "blue" => 6,
-    "grey" => 8,
-    "yellow" => 4,
-    "violet" => 7,
-    "orange" => 3,
-    "green" => 5
-   }
-
+  ColorArray = [
+    "black",
+    "brown",
+    "no_color",
+    "orange",
+    "yellow",
+    "green",
+    "blue" ,
+    "violet",
+    "grey",
+    "white"
+   ]
 end
